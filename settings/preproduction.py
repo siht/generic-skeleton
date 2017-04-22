2d18d6972b113939d2c8738d06c3bcdb1a20e3a0
carlos <radix_10339@hotmail.com>
2017-04-22 10:01:40 -0500

    now wsgi has his own directory

--- settings/preproduction.py
+++ settings/preproduction.py
@@ -1,1 +1,8 @@
 from common_es_mx import *
+
+WSGI_APPLICATION = 'wsgi.preproduction.wsgi.application'
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = "/var/www/example.com/static/"
--- settings/production.py
+++ settings/production.py
@@ -1,6 +1,8 @@
 from common_es_mx import *
 
+WSGI_APPLICATION = 'wsgi.production.wsgi.application'
+
 DEBUG = False
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = "/var/www/example.com/static/"
