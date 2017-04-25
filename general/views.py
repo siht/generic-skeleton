37cb66b87c132a37b07daf5e8c82b0aef3147d79
carlos <radix_10339@hotmail.com>
2017-04-25 08:23:15 -0500

    some minor fixes

--- general/views.py
+++ general/views.py
@@ -2,6 +2,5 @@ from django.views.generic import TemplateView
 
 class TemplateViewer(TemplateView):
     def get(self, request, template_name):
-    	template_name = template_name[:-1] if template_name.endswith('/') else template_name
         self.template_name = '{}.html'.format(template_name)
         return super(TemplateViewer, self).get(request)
--- settings/common.py
+++ settings/common.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangobower',
-    'helpers',
+    'general',
 )
 
 BOWER_INSTALLED_APPS = (
